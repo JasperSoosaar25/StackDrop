582a71808c19593c151f0da120de1341df8775cb
JasperSoosaar25 <jaspersoosaar9@gmail.com>
2026-05-08 14:17:13 +0300

    Add StackDropClient class for client mod initialization

---
+++ src/client/java/com/jasper/stackdrop/client/StackDropClient.java
@@ -0,0 +1,10 @@
+package com.jasper.stackdrop.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StackDropClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // No client-side setup needed for this mod.
+    }
+}
